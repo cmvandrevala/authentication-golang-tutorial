59076ff7f5145a4025da11abe39597c4e2b76d2b
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2017-02-21 07:44:30 -0600

    Add placeholder functionality for routes

--- main.go
+++ main.go
@@ -11,5 +11,15 @@ func main() {
   r.Handle("/", http.FileServer(http.Dir("./views/")))
   r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 
+  r.Handle("/status", notImplemented).Methods("GET")
+  r.Handle("/products", notImplemented).Methods("GET")
+  r.Handle("/products/{slug}/feedback", notImplemented).Methods("POST")
+
   http.ListenAndServe(":3000", r)
 }
+
+var notImplemented = http.HandlerFunc(
+  func(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Foo"))
+  }
+)
